c13a76d290a52eedbf74daff3e6db28fce05d984
manor1297 <manorraj@buffalo.edu>
2023-10-16 15:07:53 -0400

    fix: added deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/dragontales/',
   plugins: [react()],
   server: {
     open: true,
